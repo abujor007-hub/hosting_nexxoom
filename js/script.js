d3ed556ef59b9f5a0dddbadd318f2d73ef760500
abujor007-hub <you@example.com>
2026-08-31 17:28:24 +0600

    page

--- js/script.js
+++ js/script.js
@@ -531,3 +531,57 @@ $(document).ready(function () {
     });
 
 // faq page  end
+
+
+
+
+
+
+
+
+// Back To Top Button
+
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 300) {
+
+        backToTop.classList.remove(
+            "opacity-0",
+            "invisible"
+        );
+
+        backToTop.classList.add(
+            "opacity-100",
+            "visible"
+        );
+
+    } else {
+
+        backToTop.classList.remove(
+            "opacity-100",
+            "visible"
+        );
+
+        backToTop.classList.add(
+            "opacity-0",
+            "invisible"
+        );
+
+    }
+
+});
+
+
+backToTop.addEventListener("click", () => {
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
+});
+
+
+
